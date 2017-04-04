7189bc8f7135e9aa58e5ba41a7816ea825df8769
mnipshagen <moritz.nipshagen@gmail.com>
2017-04-04 18:56:44 +0200

    task3

---
+++ Blatt01/Aufgabe3/Fraction.java
@@ -0,0 +1,130 @@
+package Fraction;
+
+/**
+ * Speichert einen Bruch als 2 einzelne Zahlen <code>numerator</code>
+ * und <code>denominator</code>. Es werden einzelne Rechenoperationen
+ * unterstuetzt.
+ */
+public class Fraction {
+
+    // Zaehler des Bruchs
+    private int numerator;
+    // Nenner des Bruchs
+    private int denominator;
+
+    /**
+     * Konstruktor der den Bruch aus 2 uebergebenen Integers kreirt
+     * @param n Zaehler
+     * @param d Nenner
+     */
+    public Fraction(int n, int d){
+        int ggt = ggt(n, d);
+        this.numerator = n/ggt;
+        this.denominator = d/ggt;
+    }
+
+    /**
+     * Konstruktor fuer ganze zahlen, weil Integer nicht ins Schema passt
+     * @param n Der Zaehler, der Nenner ist bei Ganzzahlen 1
+     */
+    public Fraction(int n){
+        this(n, 1);
+    }
+
+    /**
+     * gibt den Zaehler aus, damit die variable private bleibt
+     * @return Numerator
+     */
+    public int getNumerator(){
+        return this.numerator;
+    }
+
+    /**
+     * Gibt den Nenner aus, damit die Variable private bleibt
+     * @return denominator
+     */
+    public int getDenominator(){
+        return this.denominator;
+    }
+
+    /**
+     * Multipliziert den Bruch mit einer ganzen Zahl. whey.
+     * @param factor der Multiplikator.
+     * @return den multiplizierten Bruch
+     */
+    public Fraction multiply(int factor){
+        int newnum = this.numerator * factor;
+
+        Fraction f = new Fraction(newnum, this.getDenominator());
+
+        return f;
+    }
+
+    /**
+     * Multiplieziert mit einem anderen Bruch. n*n und d*d
+     * @param factor der multiplikator Bruch
+     * @return Das Produkt
+     */
+    public Fraction multiply(Fraction factor){
+        int newnum = this.numerator * factor.getNumerator();
+        int newdenom = this.denominator * factor.getDenominator();
+
+        Fraction f = new Fraction(newnum, newdenom);
+
+        return f;
+    }
+
+    /**
+     * Multipliziert den Bruch mit vielen, vielen Bruechen
+     * @param factors Liste mit Faktoren
+     * @return Das (Bruttosozial)Produkt
+     */
+    public Fraction multiply(Fraction... factors){
+        Fraction f =
+                new Fraction(this.getNumerator(), this.getDenominator());
+
+        for(Fraction fs : factors){
+            f = f.multiply(fs);
+        }
+
+        return f;
+    }
+
+    /**
+     * Durch Brueche teilen ist mit dem Kehrbruch mal nehmen. whey.
+     * @param divisor Der zu drehende Bruch
+     * @return Das Produkt der entstandenen Multiplikation
+     */
+    public Fraction divide(Fraction divisor){
+        Fraction f =
+                new Fraction(divisor.getDenominator(),
+                        divisor.getNumerator());
+
+        return this.multiply(f);
+    }
+
+    /**
+     * Hilfsfunktion zum Brueche kuerzen. Berechnet den ggt.
+     * @param x die erste Zahl
+     * @param y die zweite Zahl
+     * @return ggt
+     */
+    private int ggt(int x, int y) {
+        while (y != 0) {
+            if (x > y) {
+                x = x - y;
+            } else {
+                y = y - x;
+            }
+        }
+        return x;
+    }
+
+    /**
+     * Gibt den Bruch formatiert aus
+     * @return Bruch in Schriftform
+     */
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
+}
---
+++ Blatt01/Aufgabe3/FractionTest.java
@@ -0,0 +1,90 @@
+package Fraction;
+
+/**
+ * Eine Testklasse, die alle Funktionen von <code>Fraction</code> testet
+ */
+public class FractionTest {
+
+    public static void main(String[] args) {
+
+        Fraction f1 = new Fraction(2, 3);
+        Fraction f2 = new Fraction(4, 10);
+        Fraction f3 = new Fraction(9, 90);
+        Fraction g = new Fraction(5);
+
+        // Alle Brueche werden auf korrektes kuerzen ueberprueft.
+        System.out.println("Überprüfe korrekte Instanziierung:");
+
+            if (f1.getNumerator() == 2
+                    && f1.getDenominator() == 3) {
+                System.out.println("f1 korrekt.");
+            }
+            else {
+                System.out.print("f1 Fehler! f1 = " + f1.toString());
+            }
+
+            if (f2.getNumerator() == 2
+                    && f2.getDenominator() == 5) {
+                System.out.println("f2 korrekt.");
+            }
+            else {
+                System.out.print("f2 Fehler! f2 = " + f2.toString());
+            }
+
+            if (f3.getNumerator() == 1
+                    && f3.getDenominator() == 10)
+                System.out.println("f3 korrekt.");
+            }
+            else {
+                System.out.print("f3 Fehler! f3 = " + f3.toString());
+            }
+
+            if (g.getNumerator() == 5
+                    && g.getDenominator() == 1) {
+                System.out.println("g korrekt.");
+            }
+            else {
+                System.out.print("g Fehler! g = " + g.toString());
+            }
+
+        // Alle Rechenoperationen werden einmal durchgefuehrt und es
+        // wird verglichen ob alle Ergebnisse korrekt sind. Falls nicht,
+        // werden "ist" und "sollte" Ergebnis angezeigt um Fehler zu finden.
+        System.out.println();
+        System.out.println("Teste Rechenaufgaben:");
+
+            Fraction p1 = f1.multiply(4);
+            if(p1.getNumerator() == 8) {
+                System.out.println("Ganzzahlige Multiplikation: Check!");
+            }
+            else
+              System.out.println("Ganzzahlige Multiplikation fehlgeschlagen");
+
+            Fraction p2 = f2.multiply(f3);
+            if(p2.getNumerator() == 1 && p2.getDenominator() == 25) {
+                System.out.println("Faktorielle Multiplikation: Check");
+            }
+            else
+                System.out.println("Faktorielle Multiplikation: Nope. "
+                        + "Sollte sein: 1/25. Ist: " + p2.toString());
+
+            Fraction p3 = f1.divide(f2);
+            if(p3.getNumerator() == 5 && p3.getDenominator() == 3) {
+                System.out.println("Division: Oh Yeah!");
+            }
+            else
+                System.out.println("Division: Och neee. Sollte sein:"
+                        + "5/3. Ist aber: " + p3.toString());
+
+            Fraction p4 = f1.multiply(f2, f3, g);
+            if(p4.getNumerator() == 2 && p4.getDenominator() == 15) {
+                System.out.println("Multiple Orgasmen, eeehhm, "
+                            + "Multiplikation: Ohhhh jaaaa!");
+            }
+            else
+                System.out.println("Multiple Multiplikation "
+                        + "Massiv Manipuliert! Sollte sein: 2/15. "
+                        + "Ist aber: " + p4.toString());
+    }
+
+}
