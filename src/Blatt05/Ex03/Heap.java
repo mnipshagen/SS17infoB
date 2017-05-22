327ff4b353b9ac402b9c94d123514b044fead3ce
toludwig <toludwig@uos.de>
2017-05-23 00:38:04 +0200

    ex03 working

--- src/Blatt05/Ex03/Heap.java
+++ src/Blatt05/Ex03/Heap.java
@@ -1,7 +1,145 @@
 package Blatt05.Ex03;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
- * Created by Mo on 18/05/2017.
+ * Authors: Mo Nipshagen, Tobias Ludwig
+ *
+ * A typesafe heap working with elements implementing
+ * Comparable or, alternatively ordered by a Comparator.
  */
-public class Heap<T> {
+public class Heap<T extends Comparable<T>> {
+
+    private T[] array;
+    private Comparator<T> c;
+
+    /**
+     * Constructor for setting the Comparator for the keys.
+     * @param c the Comparator
+     */
+    public Heap(Comparator<T> c) {
+        this.c = c;
+    }
+
+    /**
+     * Default (empty) constructor.
+     * Does not define a Comparator, keys are ordered by compareTo instead.
+     */
+    public Heap() { }
+
+    /**
+     * Wrapper for comparison of keys.
+     * In case Comparator c is set, use that,
+     * else, just compareTo on the keys,
+     * which are Comparables.
+     * @param key1
+     * @param key2
+     * @return value < 0 if key1 < key2, value==0 if key1==key2, value > 0 otherwise
+     */
+    private double compareKeys(T key1, T key2){
+        if(c == null){
+            return key1.compareTo(key2);
+        } else{
+            return c.compare(key1, key2);
+        }
+    }
+
+    /**
+     * Inserts an element into a heap.
+     * The key is appended as the last node and then sifted
+     * upwards until the heap criterion is fulfilled.
+     * Due to sifting, this lies in O(log n).
+     * @param key the key to be inserted
+     */
+    public void insert(T key) {
+        if(this.empty()){ // for the very first insertion
+            array = (T[]) new Comparable[]{key}; // just put the root
+            return;                              // and finish
+        }
+
+        // copyOfRange uses array.length+1 as final index (which appends null)
+        array = Arrays.copyOfRange(array, 0, array.length+1);
+        array[array.length-1] = key; // set the key as last element
+
+        // sifting upwards
+        int curi = array.length-1; // current index
+        int parent = -1;         // parent index, initially dummy
+        while (curi != 0) {      // as long as we have not reached the root
+            parent = (curi - 1) / 2; // calculate parent index
+            if (compareKeys(array[parent], key) > 0) { // if parent larger
+                array[curi] = array[parent]; // parent and child have
+                array[parent] = key;         // to swap places
+                curi = parent;
+            } else              // otherwise the key can stay in place
+                break;
+        }
+    }
+
+    /**
+     * Delete the minimum, the root node.
+     * Requires reorganizing of the array in O(log n).
+     */
+    public void del_min() {
+        // if only one element left, remove it and stop immediately
+        if(array.length == 1){
+            array = null;
+            return;
+        }
+
+        // remove the last element and set it as the new root
+        T last = array[array.length-1];
+        array = Arrays.copyOfRange(array, 0, array.length-1);
+        array[0] = last;
+
+        // rebuild the array by sifting the new root down
+        int curi = 0, childi = 1;
+        while(childi < array.length){
+            // find the smaller child
+            T c1 = array[childi];
+            T c2, cc; // cc will be the smaller of both children, the child to check
+            if(childi+1 < array.length) { // if a second (right) child exists,
+                c2 = array[childi+1];     // check for that, too
+                if(compareKeys(c1, c2) < 0) // the smaller one is assigned to cc
+                    cc = c1;
+                else{
+                    cc = c2;
+                    childi++; // take the index of the right child
+                }
+            } else
+                cc = c1;
+
+            if(compareKeys(last, cc) > 0){
+                array[curi] = array[childi];
+                array[childi] = last;
+                curi = childi;
+                childi = 2*curi +1;
+            } else
+                break;
+        }
+    }
+
+
+    /**
+     * Returns the minimum, the root node.
+     */
+    public T get_min() {
+        return array[0];
+    }
+
+    /**
+     * Heap is empty if no element left, or no element was ever inserted.
+     * @return true, if no element in heap
+     */
+    public boolean empty() {
+        return array == null || array.length == 0;
+    }
+
+    /**
+     * Return String representation of the heap as an array.
+     * @return heap as array
+     */
+    public String toString(){
+        return Arrays.toString(array);
+    }
 }
---
+++ src/Blatt05/Ex03/HeapTest.java
@@ -0,0 +1,62 @@
+package Blatt05.Ex03;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class HeapTest{
+
+    public static void main(String[]args){
+        // Test sorting for Integers
+        Integer[] array = new Integer[]{3, 4, 3, 1, 8, 6};
+        System.out.println("Sorting the Array: " + Arrays.toString(array));
+        // first transform data into a heap
+        Heap<Integer> heap_int = new Heap<Integer>();
+        for(Integer a : array) {
+            heap_int.insert(a);
+        }
+        sort(heap_int);
+
+        //----------------------------------------------------------------
+        // Now for Strings.
+        // Testing with both, lexical sorting
+        Heap<String> heap_lex = new Heap<String>();
+        // and with sorting for word length
+        Heap<String> heap_len = new Heap<String>(new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return s1.length() - s2.length(); // return < 0, if s1 shorter than s2
+            }
+        });
+
+        Heap[] heaps = new Heap[]{heap_lex, heap_len};
+
+        int i=0;
+        for(Heap heap : heaps) {
+            System.out.println(i==0 ?
+                    "\nComparison alphabetically:\n==========================" :
+                    "\nComparison of word length:\n=========================="
+            );
+            heap.insert("David");
+            heap.insert("Benjamin");
+            heap.insert("Claudia");
+            heap.insert("Abel");
+
+            sort(heap);
+            i++;
+        }
+
+    }
+
+    /**
+     * Iterative sorting using Heapsort.
+     * Does not sort in-place, but destructively.
+     */
+    public static void sort(Heap heap) {
+        System.out.println("Elements sorted ascendingly: ");
+        while(!heap.empty()){
+            System.out.println(heap.get_min() +",\t Current Heap: "+ heap.toString());
+            heap.del_min();
+        }
+    }
+}
+
