8a92d265950786b32ea01d60ca4a0cf6c2df6091
Moritz Nipshagen <moritz.nipshagen@gmail.com>
2017-06-15 22:42:07 +0200

    does not break stuff anymore. RIP all files

--- src/Blatt08/Ex04/Replace.java
+++ src/Blatt08/Ex04/Replace.java
@@ -11,11 +11,21 @@ import java.util.Arrays;
 import static Blatt08.io.FileVisitResult.*;
 
 /**
- * Created by nipsh on 15/06/2017.
+ * This class implements a file crawler to explore each file in a directory or single file and search each file
+ * for a regular expression and replaces all occurrences of it with the given String. Recursive behaviour for
+ * subtree exploration might be set via <code>-r</code> or <code>--recursive</code> arguments.
+ *
+ * Usage: <code>java Blatt08.Ex04.Replace [-r] regex_search replace [Path]</code>
+ *
+ * If no path is supplied the working directory is searched for. Per default, invisible files (starting with ".")
+ * and several file formats as well as the "src" folder are ignored.
  */
 public class Replace
 {
+    // represents the "new line" char for the system
     private static final String NL = System.getProperty("line.separator");
+    // whey debug mode
+    private static boolean debug = false;
 
     public static void main(String[] args)
     {
@@ -35,13 +45,19 @@ public class Replace
                     recursive = true;
                     i++;
                 }
+                if (Arrays.asList(new String[]{"-D", "--debug"}).contains(s))
+                {
+                    debug = true;
+                    i++;
+                }
             }
+            // check if there are two more arguments for search and replace
             if (args.length - i < 2)
             {
                 throw new IllegalArgumentException("Supply search and replace String.");
             }
-
-            if (args.length - i - 1 > 0)
+            // is there a file path specified?
+            if (args.length - i - 2 > 0)
             {
                 dir = new FileSystem(new File(args[i + 2]));
             }
@@ -49,10 +65,12 @@ public class Replace
             {
                 dir = new FileSystem(new File(System.getProperty("user.dir")));
             }
-
+            // creating the visitor with all set options and adding a blacklist
             Visitor v = new Visitor(dir.getRootPath(), recursive, args[i], args[i + 1]);
-            v.addFilter("\\..*");
+            v.addFilter("\\..*",".*\\.(java|class|pdf|html|js|css|iml|png|jpg|jpeg|jar)",".*src.*",".*out.*");
 
+            if (debug) System.out.println("Crawling through " + dir.getRootPath() + " and recursive: " + recursive);
+            // here we go
             dir.accept(v);
         }
         catch (Exception e)
@@ -61,11 +79,18 @@ public class Replace
         }
     }
 
+    /**
+     * print a manual?
+     * @param e Exception that was caught before
+     */
     private static void manual(Exception e)
     {
         e.printStackTrace();
     }
 
+    /**
+     * An inner class that does the visit work on our FileSystem
+     */
     private static class Visitor
             implements FileVisitor
     {
@@ -126,7 +151,7 @@ public class Replace
         @Override
         public FileVisitResult preVisitDirectory(File dir)
         {
-            return (!dir.equals(root)) && (skip(dir) || recursive) ? SKIP_SUBTREE : CONTINUE;
+            return (dir.equals(root) || recursive) && !skip(dir) ? CONTINUE : SKIP_SUBTREE;
         }
 
         /**
@@ -147,11 +172,10 @@ public class Replace
         }
 
         /**
-         * Called by the visiting FileSystem to visit a file with this FileVisitor.
-         * May return {@link FileVisitResult#CONTINUE} if the visiting of files and
-         * directories should continue or {@link FileVisitResult#TERMINATE} if not.
-         * {@link FileVisitResult#SKIP_SUBTREE} will have the same effect as
-         * {@link FileVisitResult#CONTINUE}.
+         * For each visited file, check whether it is on the blacklist. If not then stream the whole file line by line
+         * connected by "new line" characters into a single string.
+         * Replace all occurences of {@link #search} with {@link #replace} in that string.
+         * Finally flush to file, replacing the old one.
          *
          * @param file the file that is visited
          * @return if the visit should continue or not.
@@ -159,33 +183,46 @@ public class Replace
         @Override
         public FileVisitResult visitFile(File file)
         {
-            String content = "";
-
-            try (BufferedReader in = new BufferedReader(new FileReader(file)))
+            if (debug) System.out.println("Visiting " + file);
+            // check blacklist
+            if (!skip(file))
             {
-                String line = in.readLine();
-                while (line != null)
+                String content = "";
+                // try with resources (love that one) to read aaaaaaall the contents
+                try (BufferedReader in = new BufferedReader(new FileReader(file)))
                 {
-                    content = content.concat(NL).concat(line);
-                    line = in.readLine();
+                    String line = in.readLine();
+                    while (line != null)
+                    {
+                        content = content.concat(NL).concat(line);
+                        line = in.readLine();
+                    }
+                    if (debug) System.out.println(String.format("Read content:\n%.50s", content));
+                    // replace all the things
+                    content = content.replaceAll(search, replace);
+                    if (debug) System.out.println(String.format("\nReplaced:\n%.50s", content));
                 }
-                content = content.replaceAll(search, replace);
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                    return TERMINATE;
+                }
+                // try with resources to write to the file. no append to overwrite contents
+                try (FileWriter fw = new FileWriter(file, false))
+                {
+                    fw.write(content);
+                    if (debug) System.out.println("Flushed to file.");
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                    return TERMINATE;
+                }
+                    if (debug) System.out.println("Moving on.");
             }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-                return TERMINATE;
-            }
-            try (FileWriter fw = new FileWriter(file, false))
-            {
-                fw.write(content);
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-                return TERMINATE;
-            }
+            // and forth we go
             return CONTINUE;
+
         }
 
         /**
