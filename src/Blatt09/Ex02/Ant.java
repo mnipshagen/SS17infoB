26a6b943ad97b8a8622a63cfae6a546c79cea191
mnipshagen <moritz.nipshagen@gmail.com>
2017-06-22 00:58:57 +0200

    Done? I think

--- src/Blatt09/Ex02/Ant.java
+++ src/Blatt09/Ex02/Ant.java
@@ -1,6 +1,12 @@
-package antRace;
+package Blatt09.Ex02;
 
-import antRace.AntField.Field;
+import Blatt09.Ex02.AntField.Field;
+
+import java.util.Vector;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * An {@code Ant} is created at a specific position of an {@link AntField} with
@@ -14,34 +20,105 @@ import antRace.AntField.Field;
  * an incremented {@code stepCount} to visit the other available {@code Field}
  * elements. The Ant will run until it finds no more {@code Field} elements in
  * its neighborhood to be altered.
- * 
+ *
  * @author Mathias Menninghaus (mathias.menninghaus@uos.de)
- * 
  */
 public class Ant implements Runnable {
+    /**
+     * This static pool is responsible to schedule our might ant workforce
+     */
+    public static final ExecutorService pool = Executors.newCachedThreadPool();
+    /**
+     * Since Java does even in version 9 has no f*cking tuple class... (WTF Oracle?!)
+     * No, arrays and collections are only semi good replacements.
+     * The delta x & y to the moore neighbour fields
+     */
+    private static final Pair[] moores = {
+            new Pair(1, 1),
+            new Pair(1, -1),
+            new Pair(1, 0),
+            new Pair(-1, 1),
+            new Pair(-1, -1),
+            new Pair(-1, 0),
+            new Pair(0, 1),
+            new Pair(0, -1)
+    };
+
+    /**
+     * all the data an ant needs
+     */
+    private final AntField fields;
+    int x;
+    int y;
+    int steps;
 
 
-   /**
-    * 
-    * @param fields
-    *           the {@code AntField} on which this {@code Ant} operates
-    * @param x
-    *           x-axis value of the starting position
-    * @param y
-    *           y-axis value of the starting position
-    * @param stepCount
-    *           initial stepCount of this {@code Ant}.
-    * 
-    * @throws IllegalArgumentException
-    *            If the {@code Field} at position {@code x,y} does not exist, or
-    *            if its value is < 0
-    */
-   public Ant(AntField fields, int x, int y, int stepCount) {
-   
-   }
+    /**
+     * @param fields    the {@code AntField} on which this {@code Ant} operates
+     * @param x         x-axis value of the starting position
+     * @param y         y-axis value of the starting position
+     * @param stepCount initial stepCount of this {@code Ant}.
+     * @throws IllegalArgumentException If the {@code Field} at position {@code x,y} does not exist, or
+     *                                  if its value is < 0
+     */
+    public Ant(AntField fields, int x, int y, int stepCount) {
+        this.fields = fields;
+        this.x = x;
+        this.y = y;
+        this.steps = stepCount;
+        fields.getField(x, y).setValue(stepCount);
+    }
 
-   public void run() {
-      
-   }
+    /**
+     * This is were the magic happens.
+     * If we can move to a field in moore neighbourhood and decrease or initialise the step counter there,
+     * then we do and start a new ant thread with it.
+     * All neighbour ant threads are submitted to the {@link #pool} and their {@link Future} is saved to a vector.
+     * After we checked and started all neighbour ants, we wait for all futures to complete.
+     * We do not timeout the Futures as this is done in {@link AntRace}
+     * Thus, if this method is invoked by a different class, be careful.
+     */
+    public void run() {
+        // ants from the future. pew pew
+        Vector<Future> roboAnts = new Vector<>();
+
+        // and check the neighbours out ;)
+        for (Pair moore : moores) {
+            int nx = x + moore.x;
+            int ny = y + moore.y;
+            Field f = fields.getField(nx, ny);
+            // fields may be null if negative -> they are walls. We cannot walk on walls. No god ants here
+            if (f != null)
+                if (f.getValue() == 0 || f.getValue() > steps) {
+                    Ant ant = new Ant(fields, nx, ny, steps + 1);
+                    // you go little ant! This is your field!
+                    roboAnts.add(pool.submit(ant));
+                }
+        }
+        // and wait for our neighbours to wave back at us
+        for (Future fu : roboAnts) {
+            try {
+                if (fu != null)
+                    fu.get();
+            } catch (InterruptedException | ExecutionException e) {
+                // if we are interrupted while waiting, cancel the future we were waiting for.
+                e.printStackTrace();
+                fu.cancel(true);
+            }
+        }
+    }
+
+    /**
+     * A convenience class to represent a simple pair of ints. F*ck you {@code Oracle}
+     */
+    private static class Pair {
+        final int x;
+        final int y;
+
+        Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 
 }
--- src/Blatt09/Ex02/AntRace.java
+++ src/Blatt09/Ex02/AntRace.java
@@ -1,16 +1,65 @@
-package antRace;
+package Blatt09.Ex02;
 
-
+/**
+ * A test class for the multithreaded ant racing
+ * Tests all (at the time of implementation) fields of {@link AntFields}
+ *
+ * @author Moritz Nipshagen
+ * @author Tobias Ludwig
+ * @version 1.0
+ */
 public class AntRace implements AntFields {
 
-	public static void main(String[] args) {
-
-		AntField field = new AntField(FIELD4);
-
-		Ant ant = new Ant(field, 2, 4, 1);
-
-		new Thread(ant).start();
-
-		System.out.println(field.toString());
-	}
+    public static void main(String[] args) {
+        // we are testing all the fields!
+        AntField[] fields = {
+                new AntField(FIELD1),
+                new AntField(FIELD2),
+                new AntField(FIELD3),
+                new AntField(FIELD4),
+                new AntField(FIELD5),
+                new AntField(FIELD6)
+        };
+        // start positions for each field
+        int[][] startPos = {
+                {2,4},
+                {1,2},
+                {0,0},
+                {2,4},
+                {0,1},
+                {0,0}
+        };
+        // for each field ...
+        for (int i = 0; i < fields.length; i++) {
+            // ... set the starting point ...
+            AntField field = fields[i];
+            int x = startPos[i][0];
+            int y = startPos[i][1];
+            // ... give a nice heads up ...
+            System.out.println("Starting to fill field " + (i + 1) + " ...");
+            // (time meausres)
+            long time = System.nanoTime();
+            // ... ready, ...
+            Ant ant = new Ant(field, x, y, 1);
+            // ... steady, ...
+            Thread t = new Thread(ant);
+            // ... GO! ...
+            t.start();
+            try {
+                // ... wait a max of 10 seconds before cancelling the thread ...
+                t.join(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                // ... and tell us if we did ...
+                System.err.println("Interrupted ant racing for field " + i);
+            }
+            // ... give us all the data ...
+            System.out.printf("... done. The ant racing took %.3f milliseconds.\n\n", ((System.nanoTime() - time)/1000000.0));
+            // ... and the result
+            System.out.println(field.toString());
+        }
+        System.out.println("\nAll done. :)");
+        // shutdown thread pool
+        Ant.pool.shutdown();
+    }
 }
